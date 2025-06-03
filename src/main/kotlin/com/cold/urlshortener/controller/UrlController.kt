11c5bd3fbbcea4631e71bb1c646f7b5ee794f997
soohunee <tngjsdl036@gmail.com>
2025-06-03 17:09:33 +0900

    Remove /v1

--- src/main/kotlin/com/cold/urlshortener/controller/UrlController.kt
+++ src/main/kotlin/com/cold/urlshortener/controller/UrlController.kt
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 import java.net.URI
 
 
-@RestController("/v1")
+@RestController
 class UrlController(
     private val urlShortener: UrlShortener,
     private val urlFinder: UrlFinder
